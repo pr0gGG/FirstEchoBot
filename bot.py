ced76750bcf9a2b1e5644fffcef52bacf706bbc4
pr0gGG <smilesmilofich@gmail.com>
2022-12-07 23:26:46 +0300

    Добавил в Бота команду /help

--- bot.py
+++ bot.py
@@ -11,3 +11,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отправлю этот текст тебе в ответ!")
