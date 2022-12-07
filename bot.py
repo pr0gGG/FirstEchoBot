58db7da39698d4415880a20692d365174d0bf739
pr0gGG <smilesmilofich@gmail.com>
2022-12-07 23:23:21 +0300

    Добавил в Бота командку /start

--- bot.py
+++ bot.py
@@ -7,3 +7,7 @@ from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
