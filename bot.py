f3482c2d4af3dbe7b67393dae3a22b5b0096e7ae
pr0gGG <smilesmilofich@gmail.com>
2022-12-07 23:29:06 +0300

    Добавил в бота поллинг

--- bot.py
+++ bot.py
@@ -19,3 +19,6 @@ async def process_help_command(message: types.Message):
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
